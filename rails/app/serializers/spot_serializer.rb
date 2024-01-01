beff39eb52b5e6fdc30b28d7b36a6a97eb82aa8b
okjm3324 <okjm.yuma@gmail.com>
2024-01-01 21:39:30 +0900

    userとspotのserializer作成

---
+++ rails/app/serializers/spot_serializer.rb
@@ -0,0 +1,8 @@
+class SpotSerializer < ActiveModel::Serializer
+  attributes :id, :name, :latitude, :longitude, :address, :created_at
+  belongs_to :user, serializer: UserSerializer
+  
+  def created_at
+    object.created_at.strftime("%Y/%m/%d")
+  end
+end
