9a19e3971b1672dcc22846400a660684cf14b04b
okjm3324 <okjm.yuma@gmail.com>
2024-01-01 21:31:15 +0900

    Add:Spotのseedを作成

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+ActiveRecord::Base.transaction do
+  user1 = User.create!(name: "テスト太郎", email: "test1@example.com", password: "password", confirmed_at: Time.current)
+
+  user2 = User.create!(name: "テスト次郎", email: "test2@example.com", password: "password", confirmed_at: Time.current)
+
+  15.times do |i|
+    Spot.create!(name: "テストタイトル1-#{i}", longitude: 333333333.3333, latitude: 66666.66666, address: "安養#{i}", status: :published, user: user1)
+    Spot.create!(name: "テストタイトル2-#{i}", longitude: 333333333.3333, latitude: 66666.66666, address: "安養#{i}", status: :published, user: user2)
+  end
+end
