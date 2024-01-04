a4e3c8c144aa3d365a9a0428f7335cf3fdfddf50
okjm3324 <okjm.yuma@gmail.com>
2024-01-04 22:47:09 +0900

    Fix: rubocopを通す

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -4,7 +4,7 @@ ActiveRecord::Base.transaction do
   user2 = User.create!(name: "テスト次郎", email: "test2@example.com", password: "password", confirmed_at: Time.current)
 
   15.times do |i|
-    Spot.create!(name: "テストタイトル1-#{i}", longitude: 333333333.3333, latitude: 66666.66666, address: "安養#{i}", status: :published, user: user1)
-    Spot.create!(name: "テストタイトル2-#{i}", longitude: 333333333.3333, latitude: 66666.66666, address: "安養#{i}", status: :published, user: user2)
+    Spot.create!(name: "テストタイトル1-#{i}", longitude: 333_333_333.3333, latitude: 66666.66666, address: "安養#{i}", status: :published, user: user1)
+    Spot.create!(name: "テストタイトル2-#{i}", longitude: 333_333_333.3333, latitude: 66666.66666, address: "安養#{i}", status: :published, user: user2)
   end
 end
