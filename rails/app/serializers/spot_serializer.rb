4ccb35bbfd0331fb2c729707054adb700248581d
okjm3324 <okjm.yuma@gmail.com>
2024-01-04 22:47:09 +0900

    Fix: rubocopを通す

--- rails/app/serializers/spot_serializer.rb
+++ rails/app/serializers/spot_serializer.rb
@@ -1,7 +1,7 @@
 class SpotSerializer < ActiveModel::Serializer
   attributes :id, :name, :latitude, :longitude, :address, :created_at
   belongs_to :user, serializer: UserSerializer
-  
+
   def created_at
     object.created_at.strftime("%Y/%m/%d")
   end
