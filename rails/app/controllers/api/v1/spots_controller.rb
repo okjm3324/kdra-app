7d9424c36656e71b304c45a5af8b6c218e6e0fea
okjm3324 <okjm.yuma@gmail.com>
2024-01-04 22:42:30 +0900

    Add: ページネーションの追加、テストの追加

--- rails/app/controllers/api/v1/spots_controller.rb
+++ rails/app/controllers/api/v1/spots_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::SpotsController < Api::V1::BaseController
+  include Pagination
+
   def show
     spot = Spot.published.find(params[:id])
     render json: spot
   end
 
   def index
-
+    spots = Spot.published.order(created_at: :desc).page(params[:page] || 1).per(10).includes(:user)
+    render json: spots, meta: pagination(spots), adapter: :json
   end
 end
