9087a490fc61fa0e1a4df29f7c523221adb5d4c2
okjm3324 <okjm.yuma@gmail.com>
2024-01-06 22:32:39 +0900

    Add: ページネーションの追加 indexにpaginationを追加しました

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Box, Grid, Container } from '@mui/material'
+import { Box, Grid, Container, Pagination } from '@mui/material'
 import camelcaseKeys from 'camelcase-keys'
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import useSWR from 'swr'
 import Loading from '@/components/atoms/Loading'
 import SpotCard from '@/components/organisms/SpotCard'
+import Error from '@/components/templates/Error'
 import { styles } from '@/styles'
 import { fetcher } from '@/utils'
 
@@ -20,14 +22,20 @@ type SpotProps = {
   }
 }
 const Index: NextPage = () => {
-  const url = 'http://localhost:3000/api/v1/spots'
+  const router = useRouter()
+  const page = 'page' in router.query ? Number(router.query.page) : 1
+  const url = process.env.NEXT_PUBLIC_API_BASE_URL + '/spots/?page=' + page
 
   const { data, error } = useSWR(url, fetcher)
-  if (error) return <div>An error has occurred</div>
+  if (error) return <Error />
   if (!data) return <Loading />
 
   const spots = camelcaseKeys(data.spots)
+  const meta = camelcaseKeys(data.meta)
 
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    router.push('/?page=' + value)
+  }
   return (
     <Box css={styles.pageMinHeight} sx={{ backgroundColor: '#e6f2ff' }}>
       <Container maxWidth="md" sx={{ pt: 6 }}>
@@ -44,6 +52,13 @@ const Index: NextPage = () => {
             </Grid>
           ))}
         </Grid>
+        <Box sx={{ display: 'flex', justifyContent: 'center', py: 6 }}>
+          <Pagination
+            count={meta.totalPages}
+            page={meta.currentPage}
+            onChange={handleChange}
+          />
+        </Box>
       </Container>
     </Box>
   )
