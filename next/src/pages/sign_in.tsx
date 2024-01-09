ae3888ba852c16efb5c3e199937e1cc5028de96f
okjm3324 <okjm.yuma@gmail.com>
2024-01-09 15:35:59 +0900

    Add:サインイン機能の実装 ・useGlobalStateでログイン情報を管理 ・CurrentUserFetchコンポーネントでカレントユーザーをフェチする ・Signin画面の作成、機能の追加

--- next/src/pages/sign_in.tsx
+++ next/src/pages/sign_in.tsx
@@ -1,15 +1,11 @@
-import {
-  Box,
-  Button,
-  Container,
-  TextField,
-  Typography,
-  Stack,
-} from '@mui/material'
+import { LoadingButton } from '@mui/lab'
+import { Box, Container, TextField, Typography, Stack } from '@mui/material'
 import axios, { AxiosResponse, AxiosError } from 'axios'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { useForm, SubmitHandler, Controller } from 'react-hook-form'
+import { useUserState } from '@/hooks/useGlobalState'
 
 type SignInFormData = {
   email: string
@@ -18,12 +14,29 @@ type SignInFormData = {
 
 const SignIn: NextPage = () => {
   const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+  const [user, setUser] = useUserState()
 
   const { handleSubmit, control } = useForm<SignInFormData>({
     defaultValues: { email: '', password: '' },
   })
 
+  const validationRules = {
+    email: {
+      required: 'メールアドレスを入力してください。',
+      pattern: {
+        value:
+          /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/,
+        message: '正しい形式のメールアドレスを入力してください。',
+      },
+    },
+    password: {
+      required: 'パスワードを入力してください。',
+    },
+  }
+
   const onSubmit: SubmitHandler<SignInFormData> = (data) => {
+    setIsLoading(true)
     const url = process.env.NEXT_PUBLIC_API_BASE_URL + '/auth/sign_in'
     const headers = { 'Content-Type': 'application/json' }
 
@@ -32,10 +45,15 @@ const SignIn: NextPage = () => {
         localStorage.setItem('access-token', res.headers['access-token'])
         localStorage.setItem('client', res.headers['client'])
         localStorage.setItem('uid', res.headers['uid'])
+        setUser({
+          ...user,
+          isFetched: false,
+        })
         router.push('/')
       })
       .catch((e: AxiosError<{ error: string }>) => {
         console.log(e.message)
+        setIsLoading(false)
       })
   }
 
@@ -59,32 +77,39 @@ const SignIn: NextPage = () => {
           <Controller
             name="email"
             control={control}
-            render={({ field }) => (
+            rules={validationRules.email}
+            render={({ field, fieldState }) => (
               <TextField
                 {...field}
                 type="text"
                 label="メールアドレス"
+                error={fieldState.invalid}
+                helperText={fieldState.error?.message}
               />
             )}
           />
           <Controller
             name="password"
             control={control}
-            render={({ field }) => (
+            rules={validationRules.password}
+            render={({ field, fieldState }) => (
               <TextField
                 {...field}
                 type="password"
                 label="パスワード"
+                error={fieldState.invalid}
+                helperText={fieldState.error?.message}
               />
             )}
           />
-          <Button
+          <LoadingButton
             variant="contained"
             type="submit"
+            loading={isLoading}
             sx={{ fontWeight: 'bold', color: 'white' }}
           >
             送信する
-          </Button>
+          </LoadingButton>
         </Stack>
       </Container>
     </Box>
