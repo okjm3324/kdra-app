b259d83ab0ced162f3f4e287f02273e36cbfae25
okjm3324 <okjm.yuma@gmail.com>
2024-01-09 11:06:47 +0900

    Add:Signinページの追加 ・signinページの追加 ・ヘッダーからsigninページへ遷移リンクの追加 ・サインインできることを確認

---
+++ next/src/hooks/useMap.ts
@@ -0,0 +1,28 @@
+import { useJsApiLoader } from '@react-google-maps/api'
+import { useState } from 'react'
+
+
+
+export type Map = google.maps.Map;
+
+type Props = {
+  defaultPosition: google.maps.LatLngLiteral
+}
+
+export const useMap = ({ defaultPosition }: Props) => {
+  // googleMapsApiKeyは自分で取得したものに差し替えてください
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map',
+    process.env.GOOGLE_MAP_API_KEY,
+  })
+
+  const [map, setMap] = useState<Map | null>(null)
+
+  const onLoad = (map: Map) => {
+    const bounds = new window.google.maps.LatLngBounds(defaultPosition)
+    map.fitBounds(bounds)
+    setMap(map)
+  }
+
+  return { map, isLoaded, onLoad }
+}
