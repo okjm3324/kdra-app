e14493b9098816c3ebad5e29266e7097bde4b3c6
okjm3324 <okjm.yuma@gmail.com>
2024-01-15 03:27:56 +0900

    Add: Back detail_dramaアクションを追加
    detail_dramaアクションを作成し、dramaレコードに入れる値を取得し、jsonで返しました。

--- rails/app/serializers/drama_serializer.rb
+++ rails/app/serializers/drama_serializer.rb
@@ -1,4 +1,4 @@
 class DramaSerializer < ActiveModel::Serializer
   attributes :id
-  attributes :id, :title, :original_title, :total_episodes, :first_air_date, poster_path, :created_at
+  attributes :tmdb_id, :title, :original_title, :total_episodes, :first_air_date, poster_path, :created_at, :episode_number, :number_of_seasons
 end
