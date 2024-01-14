741dd0b5e22f52f920868f46fcff40a33cfcd5bd
okjm3324 <okjm.yuma@gmail.com>
2024-01-15 04:21:14 +0900

    Fix: Dramaテーブルのカラムを修正、追加

--- rails/db/migrate/20240110155617_create_drama.rb
+++ rails/db/migrate/20240110155617_create_drama.rb
@@ -1,8 +1,12 @@
 class CreateDrama < ActiveRecord::Migration[7.0]
   def change
     create_table :dramas do |t|
-        t.string :title, comment: "ドラマのタイトル"
-        t.string :image, comment:"イメージパス"
+        t.integer :tmdb_id, comment: "TMDB上のタイトルのid"
+        t.string :title, comment: "邦題"
+        t.string :original_title, comment: "原題"
+        t.integer :episode_number, comment: "エピソードの総数"
+        t.integer :season_number, comment: "シーズンの総数"
+        t.string :poster_path, comment:"イメージパス"
         t.timestamps
     end
   end
