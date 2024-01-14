00b912b589de0b3793dcccf7acd06ea04fac3caf
okjm3324 <okjm.yuma@gmail.com>
2024-01-15 06:34:27 +0900

    Add: Dramaに対するcreate
    フロントからバックへdramaをクリエイトする関数を作成
    動作を確認

--- rails/app/serializers/drama_serializer.rb
+++ rails/app/serializers/drama_serializer.rb
@@ -1,4 +1,4 @@
 class DramaSerializer < ActiveModel::Serializer
   attributes :id
-  attributes :tmdb_id, :title, :original_title, :total_episodes, :first_air_date, poster_path, :created_at, :episode_number, :number_of_seasons
+  attributes :tmdb_id, :title, :original_title,  :first_air_date, :poster_path, :created_at, :episode_number, :season_number
 end
