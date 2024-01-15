784e325b8c79e3f4251f5adda28c94b02e4e2eeb
okjm3324 <okjm.yuma@gmail.com>
2024-01-16 06:36:17 +0900

    Add: ドラマ検索画面をcreate画面に搭載する ・modalの作成 ・createページにボタンを設置し、クリックすると検索画面が表示されるようにした

--- next/src/components/atoms/DramaCard.tsx
+++ next/src/components/atoms/DramaCard.tsx
@@ -1,15 +1,10 @@
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import axios from 'axios';
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import axios from 'axios'
+import * as React from 'react'
 
 type DramaProps = {
   title: string
--- next/src/components/atoms/SearchSelectBox.tsx
+++ next/src/components/atoms/SearchSelectBox.tsx
@@ -1,24 +1,8 @@
-import {
-  Grid,
-  Button,
-  Box,
-  TextField,
-  Autocomplete,
-  Typography,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Card,
-  CardMedia,
-} from '@mui/material';
-import useSWR, { mutate } from "swr"
+import { Grid, Button, Box, TextField, Typography } from '@mui/material'
+import useSWR, { mutate } from 'swr'
 import { useForm, Controller } from 'react-hook-form';
 import { useState } from 'react'
-import camelcaseKeys from 'camelcase-keys'
 import { fetcher } from '@/utils'
-import Link from 'next/link'
 import type { NextPage } from 'next'
 import DramaCard from './DramaCard'
 import { LoadingButton } from '@mui/lab'
@@ -26,6 +10,8 @@ import axios, { AxiosResponse, AxiosError } from 'axios'
 import React from 'react'
 import Error from '../templates/Error'
 import { idText } from 'typescript';
+import {useTheme } from '@mui/material/styles'
+import Loading from './Loading';
 
 const SearchSelectBox = () => {
   //テスト
@@ -35,25 +21,23 @@ const SearchSelectBox = () => {
   //Draamの詳細を取得するためのステイト
   const [dramaDetail, setDramaDetail] = useState(null)
   const [selectedTmdbId, setSelectedTmdbId] = useState(null)
-  const [open, setOpen] = useState(false)
   const [keyword, setKeyword] = useState('')
   const url = `http://localhost:3000/api/v1/dramas/search_drama?keyword=${keyword}`
-  const poster_url = 'https://image.tmdb.org/t/p/w500'
   const { data, error, isValidating } = useSWR(url, fetcher)
   const isLoading = isValidating //ボタンのローディングに使用
+  //テーマカラーの取得
+  const theme = useTheme()
+  const primaryColor = theme.palette.primary.main
+
   if (error) {
     console.error(error)
   }
-  const { control, handleSubmit, setValue, getValues } = useForm({
+  const { control, setValue, getValues } = useForm({
     mode: 'onChange',
     defaultValues: {
-      single: {},
-      multi: [],
+      single: '',
     },
   })
-  const handleClickOpen = () => {
-    setOpen(true)
-  }
   //キーワード検索関数
   const onClickDataFetch = () => {
     const newKeyword = getValues().single ? getValues().single.toString() : ''
@@ -80,87 +64,106 @@ const SearchSelectBox = () => {
   return (
     <>
       <div style={{ padding: 30 }}></div>
-      <Box>
-        <Grid
-          container
-          rowSpacing={2}
-          columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
-          justifyContent="left"
-        >
-          <Grid item xs={1} sm={1} md={1} lg={1} xl={1}></Grid>
-          <Grid item xs={7} sm={7} md={7} lg={7} xl={7}>
-            <>
-              <form onSubmit={handleSubmit(handleClickOpen)}>
-                <Typography gutterBottom variant="h4" component="h2">
-                  Drama Select
-                </Typography>
-                <Controller
-                  control={control}
-                  name="single"
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      fullWidth
-                      label="drama"
-                      onChange={(event) => {
-                        setValue('single', event.target.value, {
-                          shouldValidate: true,
-                          shouldDirty: true,
-                          shouldTouch: true,
-                        })
-                      }}
-                    />
-                  )}
-                />
-                <Box sx={{ mt: 5 }} />
-              </form>
-            </>
-          </Grid>
-        </Grid>
-      </Box>
-      <LoadingButton
-        variant="contained"
-        onClick={onClickDataFetch}
-        loading={isLoading}
-        sx={{ fontWeight: 'bold', color: 'white' }}
-      >
-        検索
-      </LoadingButton>
-      <Box>
-        <Box>
-          {data &&
-            data.map((item, index) => (
-              <React.Fragment key={index}>
-                <DramaCard
-                  setDramaDetail={setDramaDetail}
-                  setSelectedTmdbId={setSelectedTmdbId}
-                  tmdbId={item.id}
-                  posterPath={item.poster_path}
-                  title={item.name}
-                  date={item.first_air_date}
-                  selected={selectedTmdbId === item.id}
-                />
-              </React.Fragment>
-            ))}
-        </Box>
-      </Box>
-
-      <p>{dramaDetail ? dramaDetail.title : 'ありません'}</p>
-      <Button
-        onClick={() => onClickCreateDrama(dramaDetail)}
-        color="primary"
-        disabled={!dramaDetail}
-        variant="contained"
+      <Typography gutterBottom variant="h5" component="h2">
+        新しいドラマの追加
+      </Typography>
+      <Box
         sx={{
-          color: 'white',
-          textTransform: 'none',
-          fontSize: 16,
-          borderRadius: 2,
-          boxShadow: 'none',
+          display: 'flex', // flexレイアウトを適用
+          alignItems: 'center',
+          margin: 'auto',
+          justifyContent: 'center',
         }}
       >
-        ドラマ追加
-      </Button>
+        <Controller
+          control={control}
+          name="single"
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="drama"
+              sx={{ width: '300px' }}
+              onChange={(event) => {
+                setValue('single', event.target.value, {
+                  shouldValidate: true,
+                  shouldDirty: true,
+                  shouldTouch: true,
+                })
+              }}
+            />
+          )}
+        />
+        <Box sx={{ mt: 5 }} />
+        <LoadingButton
+          variant="contained"
+          onClick={onClickDataFetch}
+          loading={isLoading}
+          size="large"
+          sx={{
+            height: '56px',
+            fontWeight: 'bold',
+            color: 'white',
+            marginLeft: 0.5,
+          }}
+        >
+          検索
+        </LoadingButton>
+      </Box>
+      <div style={{ padding: 10 }}></div>
+      <Box>
+        {data && data.length > 0 ? (
+          <>
+            <Box
+              sx={{
+                flexGrow: 1,
+                padding: 2,
+                bgcolor: primaryColor,
+                borderRadius: '5px',
+                margin: 1,
+              }}
+            >
+              <Grid container spacing={2}>
+                {data &&
+                  data.map((item, index) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                      <React.Fragment key={index}>
+                        <DramaCard
+                          setDramaDetail={setDramaDetail}
+                          setSelectedTmdbId={setSelectedTmdbId}
+                          tmdbId={item.id}
+                          posterPath={item.poster_path}
+                          title={item.name}
+                          date={item.first_air_date}
+                          selected={selectedTmdbId === item.id}
+                        />
+                      </React.Fragment>
+                    </Grid>
+                  ))}
+              </Grid>
+            </Box>
+            <div style={{ padding: 10 }}></div>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+              <Button
+                onClick={() => onClickCreateDrama(dramaDetail)}
+                color="primary"
+                disabled={!dramaDetail}
+                variant="contained"
+                sx={{
+                  color: 'white',
+                  textTransform: 'none',
+                  fontSize: 16,
+                  borderRadius: 2,
+                  boxShadow: 'none',
+                }}
+              >
+                ドラマ追加
+              </Button>
+            </Box>
+          </>
+        ) : (
+          <></>
+        )}
+      </Box>
     </>
   )
 }
