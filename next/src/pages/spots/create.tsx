ef06d1f0ed04a8be6d1be2e6152106fc6d1c0290
okjm3324 <okjm.yuma@gmail.com>
2024-01-20 16:41:16 +0900

    Add: 画像アップ機能の追加 ・S3への画像アップロード機能を実装 認証付きURL発行によるダイレクトアップロードを採用したため、 ドロップボックスに画像を指定した段階でS3にアップロードされます。 処理はバックエンドの/imagesのcreateアクションで認証付きURLを生成し、フロントに返しています。 キーを保存することで、画像へアクセスします。 ・フォームに仮のドロップボックスを設置しました。

--- next/src/pages/spots/create.tsx
+++ next/src/pages/spots/create.tsx
@@ -11,12 +11,15 @@ import {
   Autocomplete,
   Typography,
   FormHelperText,
+  Paper,
   Card,
   CardMedia,
+  Container,
+  styled
 } from '@mui/material'
 import axios, { AxiosResponse, AxiosError } from 'axios'
 import type { NextPage } from 'next'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 import useSWR, { mutate } from 'swr'
 import Modal from '../../components/molecules/Modal'
@@ -25,6 +28,14 @@ import CreateDramaContent from '@/components/organisms/CreateDramaContent'
 import { fetcher } from '@/utils'
 import SelectBox from '@/components/atoms/SelectBox'
 import FormSelectBox from '@/components/atoms/FormSelectBox'
+import {useDropzone} from 'react-dropzone'
+import camelcaseKeys from 'camelcase-keys'
+
+const StyledImage = styled('img')({
+  maxWidth: '100%',
+  height: 'auto',
+  objectFit: 'contain'
+})
 
 const CreateSpot: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false)
@@ -37,6 +48,57 @@ const CreateSpot: React.FC = () => {
   const isLoading: boolean = isValidating
 
   const [dramas, setDramas] = useState<Array<{ id: number; tmdb_id: number; title: string; original_title: string; episode_number: number; season_number: number; poster_path: string }>>([]);
+
+  //ドロップボックスのため
+  const [imageUrl, setImageUrl] = useState('')
+  const [imageKey, setImageKey] = useState('')
+
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach(async (file) => {
+      try {
+        // 認証情報を取得
+        const accessToken = localStorage.getItem('access-token');
+        console.log(accessToken)
+        const client = localStorage.getItem('client');
+        const uid = localStorage.getItem('uid');
+        // 認証情報をヘッダーに設定
+        const authHeaders = {
+          'access-token': accessToken,
+          'client': client,
+          'uid': uid,
+        }
+        // POSTリクエストで署名付きURLを取得
+        const response = await axios.post('http://localhost:3000/api/v1/images', {}, {
+          headers: authHeaders
+        });
+        console.log(response)
+        
+        console.log("keyは"+response.data.key)
+        console.log("認証URLは"+response.data.signed_url)
+        const key = response.data.key
+        const signedUrl = response.data.signed_url
+  console.log('アップロード開始'+ key + 'と'+ signedUrl)
+        // PUTリクエストでファイルをアップロード
+        await axios.put(signedUrl, file, {
+          headers: {
+            'Content-Type': file.type,
+          },
+        })
+ console.log('ここまでok') 
+        // GETリクエストで署名付きURLを取得して画像を表示
+        const res = await axios.get(`http://localhost:3000/api/v1/images/${key}`, {
+          headers: authHeaders
+        })
+        setImageUrl(res.data.signed_url)
+        setImageKey(key)
+        console.log('フィニッシュです')
+      } catch (error) {
+        console.error('ファイルのアップロード中にエラーが発生しました', error)
+      }
+    })
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
   useEffect(() => {
     console.log('実行')
     ;(async () => {
@@ -55,6 +117,12 @@ const CreateSpot: React.FC = () => {
         }))
         setDramas(newDramas)
         console.log(newDramas)
+        const headers = {
+          'Content-Type': 'application/json',
+          'access-token': localStorage.getItem('access-token'),
+          client: localStorage.getItem('client'),
+          uid: localStorage.getItem('uid'),
+        };
       } catch (error) {
         console.error('リクエストエラー', error)
       }
@@ -71,7 +139,7 @@ const CreateSpot: React.FC = () => {
     setModalOpen(false)
   }
 //フォームの宣言
-  const { control, handleSubmit, setValue, getValues } = useForm({
+  const { control, handleSubmit, setValue, getValues, register, formState: { errors }  } = useForm({
     mode: 'onChange',
     defaultValues: {
       single: {},
@@ -109,7 +177,6 @@ const CreateSpot: React.FC = () => {
 
     if (newValue !== null) {
       setSelectedDrama(newValue)
-
     }
   }
 
@@ -120,101 +187,148 @@ const CreateSpot: React.FC = () => {
   }
   return (
     <>
-      <div style={{ padding: 30 }}></div>
-      <Box>
-        <Grid
-          container
-          rowSpacing={2}
-          columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
-          justifyContent="left"
-        >
-          <Grid item xs={1} sm={1} md={1} lg={1} xl={1}></Grid>
-          <Grid item xs={7} sm={7} md={7} lg={7} xl={7}>
-            <>
-              <form onSubmit={handleSubmit(handleUpdateSpot)}>
-                <Typography gutterBottom variant="h4" component="h2">
-                  Drama Select
-                </Typography>
-                <Controller
-                  control={control}
-                  name="single"
-                  render={({ field }) => (
-                    <Autocomplete
-                      {...field}
-                      fullWidth
-                      options={dramas}
-                      getOptionLabel={(option) => option.title}
-                      renderInput={(params) => (
-                      <TextField {...params} label="drama title" />
-                      )}
-                      onChange={handleAutocompleteChange}
-                    />
-                  )}
-                />
-                <Box sx={{ mt: 5 }} />
-                {selectedDrama && (
-                  <>
-                    <DramaCard
-                      tmdbId={selectedDrama.tmdb_id}
-                      posterPath={selectedDrama.poster_path}
-                      title={selectedDrama.title}
-                      date={selectedDrama.first_air_date}
-                    />
-                    <Controller
-                      control={control}
-                      name="episode"
-                      render={({ field, fieldState }) => (
-                        <FormControl fullWidth error={fieldState.invalid}>
-                          <InputLabel id="area-label">地域</InputLabel>
-                          <Select
-                            labelId="area-label"
-                            label="地域" // フォーカスを外した時のラベルの部分これを指定しないとラベルとコントロール線が被る
+    <Container style={{ display: 'flex',　flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ padding: 30 }}></div>
+        <Box>
+          <Grid
+            container
+            rowSpacing={2}
+            columnSpacing={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
+            justifyContent="left"
+          >
+            <Grid item xs={1} sm={1} md={1} lg={1} xl={1}></Grid>
+            <Grid item xs={7} sm={7} md={7} lg={7} xl={7}>
+              <>
+                <form onSubmit={handleSubmit(handleUpdateSpot)}>
+                  <Typography gutterBottom variant="h4" component="h2">
+                    Drama Select
+                  </Typography>
+                  <Controller
+                    control={control}
+                    name="single"
+                    render={({ field }) => (
+                      <Autocomplete
+                        {...field}
+                        fullWidth
+                        options={dramas}
+                        getOptionLabel={(option) => option.title}
+                        renderInput={(params) => (
+                          <TextField {...params} label="drama title" />
+                        )}
+                        onChange={handleAutocompleteChange}
+                      />
+                    )}
+                  />
+                  <Box sx={{ mt: 5 }} />
+                  {selectedDrama && (
+                    <>
+                      <DramaCard
+                        tmdbId={selectedDrama.tmdb_id}
+                        posterPath={selectedDrama.poster_path}
+                        title={selectedDrama.title}
+                        date={selectedDrama.first_air_date}
+                      />
+                      <Controller
+                        control={control}
+                        name="episode"
+                        render={({ field, fieldState }) => (
+                          <FormControl fullWidth error={fieldState.invalid}>
+                            <InputLabel id="area-label">地域</InputLabel>
+                            <Select
+                              labelId="area-label"
+                              label="地域" // フォーカスを外した時のラベルの部分これを指定しないとラベルとコントロール線が被る
+                              {...field}
+                            >
+                              <MenuItem value="" sx={{color:'gray'}}>
+                                未選択
+                              </MenuItem>
+                              {Array.from({ length: selectedDrama.episode_number }, (_, index) => (
+                                  <MenuItem key={index + 1} value={index + 1}>
+                                    {index + 1}話
+                                  </MenuItem>
+                                ))
+                              }
+                            </Select>
+                            <FormHelperText>
+                              {fieldState.error?.message}
+                            </FormHelperText>
+                          </FormControl>
+                        )}
+                      />
+                      <Controller
+                        control={control}
+                        name="name"
+                        render={( {field} ) => ( 
+                         <TextField 
                             {...field}
-                          >
-                            <MenuItem value="" sx={{color:'gray'}}>
-                              未選択
-                            </MenuItem>
-                            {Array.from({ length: selectedDrama.episode_number }, (_, index) => (
-                                <MenuItem key={index + 1} value={index + 1}>
-                                  {index + 1}話
-                                </MenuItem>
-                              ))
-                            }
-                          </Select>
-                          <FormHelperText>
-                            {fieldState.error?.message}
-                          </FormHelperText>
-                        </FormControl>
-                      )}
-                    />
-                    <Controller
-                      control={control}
-                      name="name"
-                      render={( {field} ) => ( 
-                        <TextField 
-                          {...field}
-                          label="スポット名"
-                          variant="outlined"
-                        />
-                      )}
-                    />
-                  </>
-                )}
-                <input type="submit" />
-              </form>
-            </>
-          </Grid>
-        </Grid>
-      </Box>
-      <Button variant="contained" onClick={handleOpenModal}>
-        ドラマを追加する
-      </Button>
-      <Modal open={modalOpen} onClose={handleCloseModal}>
-        <CreateDramaContent setModalOpen={setModalOpen} updateDramaList={updateDramaList} />
-      </Modal>
-      <Box>
+                            label="スポット名"
+                            variant="outlined"
+                          />
+                        )}
+                      />
+                    </>
+                  )}
 
+  <label className="block mb-4">
+          <span>画像</span>
+          <img src={imageUrl}/>
+          {imageKey && <img src={imageUrl} className="h-32 m-4" />}
+          <div className="border-dashed border-2 h-32 rounded flex justify-center items-center" {...getRootProps()} >
+            <input {...getInputProps()} /><p className="block text-gray-400">Drop the files here ...</p>
+          </div>
+          <input type="hidden" {...register('key', { required: true })} defaultValue={imageKey} />
+          <small className="mb-2 text-red-600 block">{errors.key?.message && <span>This field is required</span>}</small>
+        </label>
+
+                  <input type="submit" />
+                </form>
+              </>
+            </Grid>
+          </Grid>
+        </Box>
+        <Button variant="contained" onClick={handleOpenModal}>
+          ドラマを追加する
+        </Button>
+        <Modal open={modalOpen} onClose={handleCloseModal}>
+          <CreateDramaContent setModalOpen={setModalOpen} updateDramaList={updateDramaList} />
+        </Modal>
+        <Box>
+        <Box width={180} height={180}>
+        <Paper
+          variant="outlined"
+          square
+          {...getRootProps()}
+          style={{
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            padding: 10,
+          }}
+        >
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <Typography>Drop the files here ...</Typography>
+          ) : (
+            <Typography>
+              Drag 'n' drop some files here, or click to select files
+            </Typography>
+          )}
+        </Paper>
       </Box>
+        <Card sx={{ width: 200, height: 200, overflow: 'hidden' }}>
+        <CardMedia
+          component="img"
+          image={imageUrl}
+          alt="画像"
+          sx={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover'
+          }}
+        />
+      </Card>
+      </Box>
+      </Container>
     </>
   )
 }
