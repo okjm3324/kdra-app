69f74224a71226589f492345be0808fb5f3abcb8
okjm3324 <okjm.yuma@gmail.com>
2024-02-04 05:10:52 +0900

    Fix:typeエラー

--- next/src/components/organisms/SpotDetailContent.tsx
+++ next/src/components/organisms/SpotDetailContent.tsx
@@ -14,15 +14,13 @@ const SpotDetailContent = ({
   location: any
 }) => {
   const [selectedIcon, setSelectedIcon] = useState('public')
-  const imageUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL ?  process.env.NEXT_PUBLIC_IMAGE_BASE_URL + spot?.key
-  //const naverUrl = `nmap://route/${selectedIcon}?slat=37.5657&slng=126.978&sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&dlat=${spot.latitude}&dlng=${spot.longitude}&dname=${spot.name}&appname=com.example.myapp`
- // const naverUrl = `nmap://route/${selectedIcon}?slat=37.5657&slng=126.978&sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&dlat=${spot.latitude}&dlng=${spot.longitude}&dname=%EC%98%AC%EB%A6%BC%ED%94%BD%EA%B3%B5%EC%9B%90&appname=com.example.myapp`
+  const imageUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL
+    ? process.env.NEXT_PUBLIC_IMAGE_BASE_URL + spot?.key
+    : ''
   const [naverUrl, setNaverUrl] = useState('')
   useEffect(() => {
-    const timestamp = new Date().getTime()
     setNaverUrl(`nmap://route/${selectedIcon}?slat=${location.lat}&slng=${location.lng}&sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&dlat=${spot.latitude}&dlng=${spot.longitude}&dname=${spot.name}&appname=kandramap`)
   }, [selectedIcon, spot])
-console.log(naverUrl)
   return (
     <Container>
       <Box
@@ -33,7 +31,7 @@ console.log(naverUrl)
           flexDirection: 'column',
         }}
       >
-        <Typography variant="h6">{spot.name}</Typography>
+        <Typography variant="h6">{spot?.name}</Typography>
         <Paper>
           <CardMedia
             component="img"
@@ -47,11 +45,30 @@ console.log(naverUrl)
             alt="drama image"
           />
         </Paper>
-        <Box sx={{display: 'flex', flexDirection: 'row', width: 200, justifyContent: 'space-between'}}>
-          <TrainIcon color={selectedIcon === 'public' ? 'primary' : 'disabled'} onClick={() => setSelectedIcon('public')} />
-          <DirectionsCarIcon color={selectedIcon === 'car' ? 'primary' : 'disabled'} onClick={() => setSelectedIcon('car')} />
-          <DirectionsWalkIcon color={selectedIcon === 'walk' ? 'primary' : 'disabled'} onClick={() => setSelectedIcon('walk')} />
-          <DirectionsBikeIcon color={selectedIcon === 'bicycle' ? 'primary' : 'disabled'} onClick={() => setSelectedIcon('bicycle')} />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            width: 200,
+            justifyContent: 'space-between',
+          }}
+        >
+          <TrainIcon
+            color={selectedIcon === 'public' ? 'primary' : 'disabled'}
+            onClick={() => setSelectedIcon('public')}
+          />
+          <DirectionsCarIcon
+            color={selectedIcon === 'car' ? 'primary' : 'disabled'}
+            onClick={() => setSelectedIcon('car')}
+          />
+          <DirectionsWalkIcon
+            color={selectedIcon === 'walk' ? 'primary' : 'disabled'}
+            onClick={() => setSelectedIcon('walk')}
+          />
+          <DirectionsBikeIcon
+            color={selectedIcon === 'bicycle' ? 'primary' : 'disabled'}
+            onClick={() => setSelectedIcon('bicycle')}
+          />
         </Box>
         <Link href={naverUrl}>
           <Button
