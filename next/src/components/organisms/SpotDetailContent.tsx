9478c758be03103bab7a946078f29bfcbd043038
okjm3324 <okjm.yuma@gmail.com>
2024-02-04 05:05:00 +0900

    Fix:typeエラー

--- next/src/components/organisms/SpotDetailContent.tsx
+++ next/src/components/organisms/SpotDetailContent.tsx
@@ -14,7 +14,7 @@ const SpotDetailContent = ({
   location: any
 }) => {
   const [selectedIcon, setSelectedIcon] = useState('public')
-  const imageUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL + spot.key
+  const imageUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL ?  process.env.NEXT_PUBLIC_IMAGE_BASE_URL + spot?.key
   //const naverUrl = `nmap://route/${selectedIcon}?slat=37.5657&slng=126.978&sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&dlat=${spot.latitude}&dlng=${spot.longitude}&dname=${spot.name}&appname=com.example.myapp`
  // const naverUrl = `nmap://route/${selectedIcon}?slat=37.5657&slng=126.978&sname=%EC%84%9C%EC%9A%B8%EB%8C%80%ED%95%99%EA%B5%90&dlat=${spot.latitude}&dlng=${spot.longitude}&dname=%EC%98%AC%EB%A6%BC%ED%94%BD%EA%B3%B5%EC%9B%90&appname=com.example.myapp`
   const [naverUrl, setNaverUrl] = useState('')
