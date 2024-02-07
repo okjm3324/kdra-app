52a2076040da459e38918e2489f9d3119b62c537
okjm3324 <okjm.yuma@gmail.com>
2024-02-07 19:04:10 +0900

    Test: cors設定をハードコーディングでテストf

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Settings.front_domain
+    origins 'https://kdra-app.vercel.app'
 
     resource "*",
              headers: :any,
