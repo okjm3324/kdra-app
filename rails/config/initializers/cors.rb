38ea58f9c24ef96394c96037cfc46a7c1832ec12
okjm3324 <okjm.yuma@gmail.com>
2024-02-08 00:47:45 +0900

    Fix:フロントエンドのオリジンを環境変数とする

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://kdra-app.vercel.app'
+    origins ENV['FRONTEND_ORIGIN'] || 'http://localhost:3000'
 
     resource "*",
              headers: :any,
